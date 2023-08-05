67c9be6454c97e4ec1110650f588d6566afc199a
Shivam27855 <120546542+Shivam27855@users.noreply.github.com>
2023-08-05 15:35:08 +0530

    Add files via upload

---
+++ src/test/java/com/in28minutes/microservices/tmkocEurekaNamingServer/TmkocEurekaNamingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.in28minutes.microservices.tmkocEurekaNamingServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TmkocEurekaNamingServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
